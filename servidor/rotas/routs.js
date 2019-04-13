9f34146f0595b824f2e6002f2fb05883c26a4c95
nEzequiel <ezequielneves011@hotmail.com>
2019-04-13 11:33:05 -0300

    routes

---
+++ servidor/rotas/routs.js
@@ -0,0 +1,14 @@
+
+const middlewares=require("../middlewares/middlewares")
+
+
+//--- Rotas disponibilizadas pela API.
+module.exports=function(app){
+    app.post("/makelogin",middlewares.makeLogin),
+    app.get("/lastposts",middlewares.getLastPosts),
+    app.get("/lastposts/:id",middlewares.getPostInfo),
+    app.get("/allposts/:page",middlewares.getAllPosts),
+    app.get("/mainmovieserie",middlewares.getMainMovieSerie)
+    app.get("/getPage/:pagina",middlewares.getPageOfMovies)
+}
+
